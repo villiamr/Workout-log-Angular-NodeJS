3b88cc6847da68477d21eb2dc939873e6a9db59e
Villiam Rådström <villiamradstrom96@gmail.com>
2019-05-16 09:17:26 +0200

    added datePipe
    Added datePipe to show date yyyy-mm-dd in log.

--- workout/src/app/get/get.component.ts
+++ workout/src/app/get/get.component.ts
@@ -1,22 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../service/api.service';
+import { DatePipe } from '@angular/common';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-get',
   templateUrl: './get.component.html',
-  styleUrls: ['./get.component.css']
+  styleUrls: ['./get.component.css'],
+  providers: [DatePipe]
 })
 export class GetComponent implements OnInit {
   workouts: any;
   panelOpenState = false;
+  myDate = new Date();
+  chart = [];
 
-  constructor(private http: HttpClient) { }
+
+
+  constructor(private http: HttpClient, private datePipe: DatePipe) { 
+  }
 
   ngOnInit() {
       this.http.get('http://localhost:4000/api').subscribe(data => {
       this.workouts = data;
       console.log(data);
+
+     // this.dataSource.paginator = this.MatPaginator;
     });
   }
+  
 }
